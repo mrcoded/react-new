add4f1952f08ab63b6f55b993de0a6d6ea448c62
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-03 02:01:39 -0800

    feat: set Card.jsx components to accept children class styles and added Card as a wrapper container to ExpenseItem and Expenses

--- react-app/src/components/Card.jsx
+++ react-app/src/components/Card.jsx
@@ -1,3 +1,10 @@
-function Card() {
-  return <div className="card"></div>;
+import "./Card.css";
+
+function Card(props) {
+  //allow the styles to apply to our custom Card contanier
+  const classes = "card " + props.className;
+
+  return <div className={classes}>{props.children}</div>;
 }
+
+export default Card;
--- react-app/src/components/ExpensesItem.jsx
+++ react-app/src/components/ExpensesItem.jsx
@@ -1,15 +1,16 @@
 import "../components/ExpensesItem.css";
 import ExpensesDate from "./ExpensesDate";
+import Card from "./Card";
 
 function ExpensesItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpensesDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
