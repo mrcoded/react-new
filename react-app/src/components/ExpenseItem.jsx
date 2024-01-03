7b8c4b78fd8ff9881b0c459ce156820a5691edb7
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-03 00:41:42 -0800

    added mock data for title, amount, and date and refactored expenseItem page

--- react-app/src/components/ExpenseItem.jsx
+++ react-app/src/components/ExpenseItem.jsx
@@ -1,18 +1,20 @@
 import "../components/ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
-    <div>
-      <div className="expense-item">
-        <div>{expenseDate.toDateString()}</div>
-        <div className="expense-item__description">
-          <h2>{expenseTitle}</h2>
-          <div className="expense-item__price">${expenseAmount}</div>
-        </div>
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
