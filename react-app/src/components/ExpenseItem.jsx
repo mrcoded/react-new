7b129e5f3c21c7d1961c5a8aba59a76dbe3ca86a
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-02 20:48:09 -0800

    added expenseItem css files

--- react-app/src/components/ExpenseItem.jsx
+++ react-app/src/components/ExpenseItem.jsx
@@ -1,3 +1,21 @@
+import "../components/ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h2>Hello</h2>;
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
+  return (
+    <div>
+      <div className="expense-item">
+        <div>{expenseDate.toDateString()}</div>
+        <div className="expense-item__description">
+          <h2>{expenseTitle}</h2>
+          <div className="expense-item__price">${expenseAmount}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default ExpenseItem;
