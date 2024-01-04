6d07202b73ac6c7e72e3073ca3a0ad88e2eb6c85
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-03 23:36:41 -0800

    feat: passed filteredExpenses as prop to ExpensesChart

--- react-app/src/components/Expenses/Expenses.jsx
+++ react-app/src/components/Expenses/Expenses.jsx
@@ -4,6 +4,7 @@ import "../Expenses/Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "../Chart/ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -22,6 +23,7 @@ function Expenses(props) {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
