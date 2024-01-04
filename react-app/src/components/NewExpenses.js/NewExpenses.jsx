c83bf9228fc75d0a17f1fed950ee40811c00fb02
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-03 17:20:02 -0800

    feat: added a 'saveExpenseDataHandler' function ti NewExpenses component and pass it to child component

--- react-app/src/components/NewExpenses.js/NewExpenses.jsx
+++ react-app/src/components/NewExpenses.js/NewExpenses.jsx
@@ -2,9 +2,16 @@ import ExpenseForm from "./ExpenseForm";
 import "./NewExpenses.css";
 
 const NewExpenses = () => {
+  const saveExpenseDataHandler = (enteredData) => {
+    const expenseData = {
+      ...enteredData,
+      id: Math.random().toString(),
+    };
+    console.log(expenseData);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
