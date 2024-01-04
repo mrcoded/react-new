f8d0ef99fecc81b225d69e1be03ea4e737ebdaef
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-03 20:49:38 -0800

    extracted filteredExpenses into expenseContext function

--- react-app/src/components/Expenses/Expenses.jsx
+++ react-app/src/components/Expenses/Expenses.jsx
@@ -15,23 +15,27 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  let expensesContent = <p>No expense found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpensesItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpensesItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-      )
+      {expensesContent}
     </Card>
   );
 }
-
 export default Expenses;
