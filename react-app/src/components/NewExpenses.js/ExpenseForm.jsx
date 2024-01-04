1a593fe8d5fc11f619dbeb4d7440cdf50992d7de
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-03 16:39:07 -0800

    feat: added submitHandler function to ExpenseForm.jsx

--- react-app/src/components/NewExpenses.js/ExpenseForm.jsx
+++ react-app/src/components/NewExpenses.js/ExpenseForm.jsx
@@ -5,24 +5,46 @@ const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+  //   const [expenseInput, setExpenseInput] = useState({
+  //     enteredTitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
 
   const handleTitleChange = (e) => {
     setEnteredTitle(e.target.value);
-    console.log(e, "title change");
+    //multiple useState object***********
+    // setExpenseInput({ ...expenseInput, enteredTitle: e.target.value });
+    //if input depends on previous state.***********
+    // setExpenseInput((prevState) => {
+    //   return { ...prevState, enteredTitle: e.target.value };
+    // });
   };
 
   const amountChangeHandler = (e) => {
     setEnteredAmount(e.target.value);
-    console.log(e, "amount change");
+    // setExpenseInput({ ...expenseInput, enteredAmount: e.target.value });
   };
 
   const dateChangeHandler = (e) => {
     setEnteredDate(e.target.value);
-    console.log(e, "date change");
+    // setExpenseInput({ ...expenseInput, enteredDate: e.target.value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const expenseDate = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseDate);
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
@@ -40,7 +62,7 @@ const ExpenseForm = () => {
         <div className="new-expense__control">
           <label>Date</label>
           <input
-            type="number"
+            type="date"
             min="2019-01-01"
             step="2022-12-31"
             onChange={dateChangeHandler}
