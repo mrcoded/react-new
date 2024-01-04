95c7a6a58278bf4302797c1fffdade38c8dfaeba
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-04 01:52:24 -0800

    feat: created a state t check if input is valid and altered label text if NOT

--- reactjs-styling/src/components/CourseGoals/CourseInput/CourseInput.jsx
+++ reactjs-styling/src/components/CourseGoals/CourseInput/CourseInput.jsx
@@ -5,6 +5,7 @@ import "./CourseInput.css";
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
@@ -13,7 +14,10 @@ const CourseInput = (props) => {
   const formSubmitHandler = (event) => {
     event.preventDefault();
 
-    if (enteredValue.trim().length === 0) return;
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
 
     props.onAddGoal(enteredValue);
   };
@@ -21,7 +25,7 @@ const CourseInput = (props) => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
