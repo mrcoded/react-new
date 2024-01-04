352f54dadab0742367f405a79c9d752015169273
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-03 23:33:12 -0800

    feat: updated max value in Chart.js

--- react-app/src/components/Chart/Chart.jsx
+++ react-app/src/components/Chart/Chart.jsx
@@ -2,13 +2,16 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
+  const dataPointsValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMax = Math.max(...dataPointsValues);
+
   return (
     <div className="chart">
       {props.dataPoints.map((data) => (
         <ChartBar
           key={data.label}
           value={data.value}
-          maxValue={null}
+          maxValue={totalMax}
           label={data.label}
         />
       ))}
