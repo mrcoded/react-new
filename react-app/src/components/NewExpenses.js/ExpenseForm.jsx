efb7451d6cbc71a35dc97b8044bd6ca2b9f155a3
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-03 16:13:58 -0800

    feat: added useState to amount and date change handler

--- react-app/src/components/NewExpenses.js/ExpenseForm.jsx
+++ react-app/src/components/NewExpenses.js/ExpenseForm.jsx
@@ -3,10 +3,22 @@ import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
   const handleTitleChange = (e) => {
     setEnteredTitle(e.target.value);
     console.log(e, "title change");
+  };
+
+  const amountChangeHandler = (e) => {
+    setEnteredAmount(e.target.value);
+    console.log(e, "amount change");
+  };
+
+  const dateChangeHandler = (e) => {
+    setEnteredDate(e.target.value);
+    console.log(e, "date change");
   };
 
   return (
@@ -18,11 +30,21 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="number" min="2019-01-01" step="2022-12-31" />
+          <input
+            type="number"
+            min="2019-01-01"
+            step="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
 
