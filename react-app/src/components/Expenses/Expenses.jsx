df8c53172847e355a356cec7750269fffb012a89
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-03 20:25:13 -0800

    feat: added filteredExpenses to filter expenses items

--- react-app/src/components/Expenses/Expenses.jsx
+++ react-app/src/components/Expenses/Expenses.jsx
@@ -11,13 +11,17 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items.map((expense, i) => (
+      {filteredExpenses.map((expense) => (
         <ExpensesItem
           key={expense.id}
           title={expense.title}
