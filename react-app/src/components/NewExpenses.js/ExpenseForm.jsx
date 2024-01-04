cf8dc1e5c5a5510f2c315ede2e1fcb5b55e675be
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-03 16:57:00 -0800

    feat: added two-way bind to clear input ater submit

--- react-app/src/components/NewExpenses.js/ExpenseForm.jsx
+++ react-app/src/components/NewExpenses.js/ExpenseForm.jsx
@@ -41,6 +41,9 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseDate);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
@@ -48,7 +51,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={handleTitleChange} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={handleTitleChange}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -56,6 +63,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -65,6 +73,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             step="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
