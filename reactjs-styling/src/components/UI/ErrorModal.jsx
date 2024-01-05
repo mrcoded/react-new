14c625cc9fb59da322140b6696f994cbe34062b1
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-05 01:43:41 -0800

    feat: added reactPortals to positon the overlay and backdrop in the DOM

--- reactjs-styling/src/components/UI/ErrorModal.jsx
+++ reactjs-styling/src/components/UI/ErrorModal.jsx
@@ -1,6 +1,7 @@
 import Card from "./Card/Card";
 import Button from "./Button/Button";
 import styles from "./ErrorModal.module.css";
+import ReactDom from "react-dom";
 
 const Backdrop = (props) => {
   <div className={styles.backdrop} onClick={props.onConfirm} />;
@@ -21,7 +22,23 @@ const ModalOverlay = (props) => {
 };
 
 const ErrorModal = (props) => {
-  return <>{}</>;
+  //React Portal - to position a component in the dom
+  return (
+    <>
+      {ReactDom.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop")
+      )}
+      {ReactDom.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay")
+      )}
+    </>
+  );
 };
 
 export default ErrorModal;
