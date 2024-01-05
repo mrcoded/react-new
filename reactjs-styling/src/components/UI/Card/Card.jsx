0da3a29dba97c59a514e6175ade1d1fb1200b67f
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-04 18:01:55 -0800

    chore: cleaned up card.jsx

--- reactjs-styling/src/components/UI/Card/Card.jsx
+++ reactjs-styling/src/components/UI/Card/Card.jsx
@@ -1,10 +1,10 @@
-import "./Card.module.css";
+import classes from "./Card.module.css";
 
 function Card(props) {
   //allow the styles to apply to our custom Card contanier
-  const classes = "card " + props.className;
+  // const classes = "card " + props.className;
 
-  return <div className={classes}>{props.children}</div>;
+  return <div className={classes.card}>{props.children}</div>;
 }
 
 export default Card;
