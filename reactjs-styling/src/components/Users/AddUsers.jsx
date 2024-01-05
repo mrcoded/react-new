dc1215875b3b1cda794af25de837e324ee2bb1b8
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-04 18:51:11 -0800

    feat: add usestate and onchange handler function for username and age

--- reactjs-styling/src/components/Users/AddUsers.jsx
+++ reactjs-styling/src/components/Users/AddUsers.jsx
@@ -1,20 +1,32 @@
+import { useState } from "react";
 import Button from "../UI/Button/Button";
 import Card from "../UI/Card/Card";
 import classes from "../Users/AddUser.module.css";
 
 const AddUsers = (props) => {
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState(0);
+
   const addUserHandler = (e) => {
     e.preventDefault();
+  };
+
+  const usernameChangeHandler = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const ageChangeHandler = (e) => {
+    setUsername(e.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameChangeHandler} />
 
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageChangeHandler} />
 
         <Button type="submit">Add User</Button>
       </form>
