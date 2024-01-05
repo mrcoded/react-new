a24601a3fa85570aacaad6f2eafa113aa0fb3288
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-05 01:34:00 -0800

    feat: added some divs to index.html and extracted the errormodal into two functions

--- reactjs-styling/src/components/UI/ErrorModal.jsx
+++ reactjs-styling/src/components/UI/ErrorModal.jsx
@@ -2,23 +2,26 @@ import Card from "./Card/Card";
 import Button from "./Button/Button";
 import styles from "./ErrorModal.module.css";
 
+const Backdrop = (props) => {
+  <div className={styles.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
+  <Card className={styles.modal}>
+    <header className={styles.header}>
+      <h2>{props.title}</h2>
+    </header>
+    <div className={styles.content}>
+      <p>{props.message}</p>
+    </div>
+    <footer className={styles.actions}>
+      <Button onClick={props.onConfirm}>Okay</Button>
+    </footer>
+  </Card>;
+};
+
 const ErrorModal = (props) => {
-  return (
-    <>
-      <div className={styles.backdrop} onClick={props.onConfirm} />
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </>
-  );
+  return <>{}</>;
 };
 
 export default ErrorModal;
