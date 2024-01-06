2588414e6a3ad875c0ab438f992f722f1668e9ff
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-06 03:09:45 -0800

    feat: added input component for email

--- react-context-reducer/src/components/Login/Login.jsx
+++ react-context-reducer/src/components/Login/Login.jsx
@@ -3,6 +3,7 @@ import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 import AuthContext from '../../store/auth-context';
+import Input from '../UI/Input/Input';
 
 const Login = () => {
   const AuthCtx = useContext(AuthContext)
@@ -113,20 +114,7 @@ const Login = () => {
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${emailState.isValid === false ? classes.invalid : ''
-            }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            // value={enteredEmail}
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
+        <Input id="email" label="E-mail" type="email" isValid={emailIsValid} value={emailState.value} onChange={emailChangeHandler} onBlur={validateEmailHandler} />
         <div
           className={`${classes.control} ${pwdState === false ? classes.invalid : ''
             }`}
@@ -145,8 +133,8 @@ const Login = () => {
             Login
           </Button>
         </div>
-      </form>
-    </Card>
+      </form >
+    </Card >
   );
 };
 
