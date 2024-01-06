b009dac2ecb3f36ef16ab013b78ded51287f239a
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-05 20:09:47 -0800

    chore: cleaned up component extension

--- react-context-reducer/src/components/Login/Login.jsx
+++ react-context-reducer/src/components/Login/Login.jsx
@@ -12,9 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(()=> {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    //use debouncing to to only get the final input
+    setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    //cleanup function
+    return ()=> {
+      
+    }
 
   }, [enteredEmail, enteredPassword])
 
