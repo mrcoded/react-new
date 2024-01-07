9e0a9d7802e91c40e61d1fa71969545408274e2e
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-06 18:04:38 -0800

    styles: added svg icon to CartIcon.js and added styles to HeaderCardButton.module.css

---
+++ react-foodorder-project/src/components/Layout/HeaderCardButton.jsx
@@ -0,0 +1,12 @@
+
+const HeaderCardButton = (props) => {
+    return (
+        <button>
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+    )
+}
+
+export default HeaderCardButton
