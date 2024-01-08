371429f19c435bf42f79c01385aaf83fb9e31d1c
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-08 03:10:24 -0800

    feat: added ref o Mealitemform component and forwardRef to input.jsx component to use ref on custom component

--- react-foodorder-project/src/components/Meals/MealItem/MealItemForm.jsx
+++ react-foodorder-project/src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,10 +1,17 @@
+import { useRef } from 'react'
 import Input from '../../Ui/Input'
 import styles from './MealItemForm.module.css'
 
 const MealItemForm = (props) => {
+    const amountInputRef = useRef()
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+    }
+
     return (
-        <form className={styles.form}>
-            <Input label="Amount" input={{
+        <form className={styles.form} onSubmit={submitHandler}>
+            <Input ref={amountInputRef} label="Amount" input={{
                 id: "amount_" + props.id,
                 type: "number",
                 min: "1",
