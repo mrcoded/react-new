fceebad46e7206ee0e57123dbd7686a4058a04c7
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-08 01:27:05 -0800

    chore: removed conflicting styles in app and index.css

--- react-foodorder-project/src/components/Meals/MealItem/MealItemForm.jsx
+++ react-foodorder-project/src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,9 +1,10 @@
+import Input from '../../Ui/Input'
 import styles from './MealItemForm.module.css'
 
 const MealItemForm = (props) => {
     return (
         <form className={styles.form}>
-            <input />
+            {/* <Input label="Amount" input={{}}> */}
             <button>+ Add</button>
         </form>
     )
