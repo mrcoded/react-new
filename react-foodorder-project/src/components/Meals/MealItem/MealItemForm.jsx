40f69d6268812d4104e4c808b2de8fd4f5b880ce
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-08 03:17:30 -0800

    feat: added usestate to check is valid and guard clause for entered amount

--- react-foodorder-project/src/components/Meals/MealItem/MealItemForm.jsx
+++ react-foodorder-project/src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,12 +1,25 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import Input from '../../Ui/Input'
 import styles from './MealItemForm.module.css'
 
 const MealItemForm = (props) => {
+    const [amountIsValid, setAmountIsValid] = useState(true)
+
     const amountInputRef = useRef()
 
     const submitHandler = (e) => {
         e.preventDefault()
+
+        const enteredAmount = amountInputRef.current.value
+        //always a string
+        const enteredAmountNumber = +enteredAmount
+
+        if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+            setAmountIsValid(false)
+            return
+        }
+
+
     }
 
     return (
@@ -20,6 +33,7 @@ const MealItemForm = (props) => {
                 defaultValue: "1"
             }} />
             <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
         </form>
     )
 }
