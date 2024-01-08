feaafe0250cf40dc650d6a1e9a9ca3a4b5df50db
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-08 01:29:34 -0800

    feat: added inout values to mealitem form component

--- react-foodorder-project/src/components/Meals/MealItem/MealItemForm.jsx
+++ react-foodorder-project/src/components/Meals/MealItem/MealItemForm.jsx
@@ -4,7 +4,14 @@ import styles from './MealItemForm.module.css'
 const MealItemForm = (props) => {
     return (
         <form className={styles.form}>
-            {/* <Input label="Amount" input={{}}> */}
+            <Input label="Amount" input={{
+                id: "amount",
+                type: "number",
+                min: "1",
+                max: "5",
+                step: "1",
+                defaultValue: "1"
+            }} />
             <button>+ Add</button>
         </form>
     )
