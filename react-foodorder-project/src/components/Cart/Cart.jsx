4e11b8528bd12d58a148e99c753cb7fae499fb4e
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-08 02:01:54 -0800

    imported Cart

--- react-foodorder-project/src/components/Cart/Cart.jsx
+++ react-foodorder-project/src/components/Cart/Cart.jsx
@@ -1,10 +1,11 @@
 import styles from './Cart.module.css'
+import Modal from '../Ui/Modal'
 
 const Cart = (props) => {
     const cartItems = [{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map(item => <li>{item.name}</li>)
 
     return (
-        <div>
+        <Modal>
             <ul className={styles['cart-items']}>{cartItems}</ul>
             <div className={styles.total}>
                 <span>Total Amount</span>
@@ -14,7 +15,8 @@ const Cart = (props) => {
                 <button className={styles['button-alt']}>Close</button>
                 <button className={styles.button}>Order</button>
             </div>
-        </div>
+        </Modal>
+
     )
 }
 
