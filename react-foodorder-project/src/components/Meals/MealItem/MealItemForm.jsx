d942add68a73e247cf3a3638373e52abe082da08
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-08 00:35:56 -0800

    feat: created MealItemForm component

---
+++ react-foodorder-project/src/components/Meals/MealItem/MealItemForm.jsx
@@ -0,0 +1,12 @@
+import styles from './MealItemForm.modules.css'
+
+const MealItemForm = (props) => {
+    return (
+        <form className={styles.form}>
+            <input />
+            <button>+ Add</button>
+        </form>
+    )
+}
+
+export default MealItemForm
