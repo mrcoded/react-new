f572cb16d2c0efe616ca38e94c4c06d6e91418d5
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-08 02:49:04 -0800

    feat: setup cartprovider component and added Cart context to headercartbutton

--- react-foodorder-project/src/components/Layout/HeaderCartButton.jsx
+++ react-foodorder-project/src/components/Layout/HeaderCartButton.jsx
@@ -1,13 +1,20 @@
+import { useContext } from "react"
 import CartIcon from "../Cart/CartIcon"
 import styles from './HeaderCartButton.module.css'
+import CartContext from "../../store/cart-context"
 
 const HeaderCartButton = (props) => {
+    const cardCtx = useContext(CartContext)
+
+    const numberOfCartItems = cardCtx.items.reduce((curNum, item) => {
+        return curNum + item.amount
+    }, 0)
     return (
-        <button className={styles.button} onClick={props.onClick}>
+        <button className={styles.button} onClick={props.onClick} >
             <span className={styles.icon}><CartIcon /></span>
             <span>Your Cart</span>
-            <span className={styles.badge}>3</span>
-        </button>
+            <span className={styles.badge}>{numberOfCartItems}</span>
+        </button >
     )
 }
 
