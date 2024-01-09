bd4d7bc3363b1e3fcbf904470ce86d5c6f9b599b
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-09 01:52:24 -0800

    feat: wrapped promised with async and await function

--- react-http/src/App.jsx
+++ react-http/src/App.jsx
@@ -6,22 +6,24 @@ import MoviesList from './components/MoviesList';
 function App() {
   const [movies, setMovies] = useState([])
 
-  function fetchMoviesHandler() {
-    fetch(`https://swapi.dev/api/films/`)
-      .then(res => {
-        return res.json()
-      }).then(data => {
-        const movieData = data.results.map(data => {
-          return {
-            id: data.episode_id,
-            title: data.title,
-            openingText: data.opening_crawl,
-            releaseDate: data.release_date
-          }
-        })
-        setMovies(data.results)
-        console.log(movieData);
-      })
+  async function fetchMoviesHandler() {
+    const response = await fetch(`https://swapi.dev/api/films/`)
+    const data = await response.json()
+    // .then(res => {
+    //   return res.json()
+    // })
+    // .then(data => {
+    const movieData = data.results.map(data => {
+      return {
+        id: data.episode_id,
+        title: data.title,
+        openingText: data.opening_crawl,
+        releaseDate: data.release_date
+      }
+    })
+    setMovies(movieData)
+    console.log(movieData);
+
   }
 
   return (
