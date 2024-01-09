16f6d8ac6bdeeacb7df23d688286de0cc19d0954
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-08 18:18:28 -0800

    chore

--- react-foodorder-project/src/components/Meals/MealItem/MealItem.jsx
+++ react-foodorder-project/src/components/Meals/MealItem/MealItem.jsx
@@ -19,7 +19,8 @@ const MealItem = (props) => {
 
     return (
         <li className={styles.meal}>
-            <div><h3>{props.name}</h3>
+            <div>
+                <h3>{props.name}</h3>
                 <div className={styles.description}>{props.description}</div>
                 <div className={styles.price}>{price}</div>
                 <div>
