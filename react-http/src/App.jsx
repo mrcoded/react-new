45242a7abff2839744915f5606042eb248ee66dd
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-09 01:43:11 -0800

    feat: used 'fetch' to get data from swapi movies api on app component

--- react-http/src/App.jsx
+++ react-http/src/App.jsx
@@ -1,31 +1,36 @@
-import React from 'react';
 
+import React, { useState } from 'react';
 import './App.css';
 import MoviesList from './components/MoviesList';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([])
+
+  function fetchMoviesHandler() {
+    fetch(`https://swapi.dev/api/films/`)
+      .then(res => {
+        return res.json()
+      }).then(data => {
+        const movieData = data.results.map(data => {
+          return {
+            id: data.episode_id,
+            title: data.title,
+            openingText: data.opening_crawl,
+            releaseDate: data.release_date
+          }
+        })
+        setMovies(data.results)
+        console.log(movieData);
+      })
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
