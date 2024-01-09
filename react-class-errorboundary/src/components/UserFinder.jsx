00455559aa4b80e05e3e1a318a5a3a6e7a1eb4e8
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-09 00:44:24 -0800

    feat: added UsersContext to app.jsx

--- react-class-errorboundary/src/components/UserFinder.jsx
+++ react-class-errorboundary/src/components/UserFinder.jsx
@@ -1,56 +1,76 @@
 import { Fragment, Component } from 'react';
+
 import Users from './Users';
 import classes from './UserFinder.module.css';
-import UsersContext from '../store/users-context'
+import UsersContext from '../store/users-context';
+// import ErrorBoundary from './ErrorBoundary';
 
 class UserFinder extends Component {
-    //can only be called once
-    static contextType = UsersContext
+    static contextType = UsersContext;
 
     constructor() {
-        super()
+        super();
         this.state = {
             filteredUsers: [],
-            searchTerm: ''
-        }
+            searchTerm: '',
+        };
     }
 
     componentDidMount() {
-        //only runs once
-        //send http requests....
-        this.setState({ filteredUsers: this.context.users })
+        // Send http request...
+        this.setState({ filteredUsers: this.context.users });
     }
 
     componentDidUpdate(prevProps, prevState) {
         if (prevState.searchTerm !== this.state.searchTerm) {
-
             this.setState({
                 filteredUsers: this.context.users.filter((user) =>
-                    user.name.includes(this.state.searchTerm))
-            })
+                    user.name.includes(this.state.searchTerm)
+                ),
+            });
         }
     }
 
-    searchChangeHandler(e) {
-        this.setState({
-            searchTerm: e.target.value
-        })
+    searchChangeHandler(event) {
+        this.setState({ searchTerm: event.target.value });
     }
 
     render() {
         return (
             <Fragment>
-                {/* <UsersContext.Consumer> */}
                 <div className={classes.finder}>
-                    <input type='search'
-                        onChange={this.searchChangeHandler.bind(this)} />
+                    <input type='search' onChange={this.searchChangeHandler.bind(this)} />
                 </div>
+                {/* <ErrorBoundary> */}
                 <Users users={this.state.filteredUsers} />
-                {/* </UsersContext.Consumer> */}
+                {/* </ErrorBoundary> */}
             </Fragment>
         );
     }
 }
 
+// const UserFinder = () => {
+//   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+//   const [searchTerm, setSearchTerm] = useState('');
+
+//   useEffect(() => {
+//     setFilteredUsers(
+//       DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+//     );
+//   }, [searchTerm]);
+
+//   const searchChangeHandler = (event) => {
+//     setSearchTerm(event.target.value);
+//   };
+
+//   return (
+//     <Fragment>
+//       <div className={classes.finder}>
+//         <input type='search' onChange={searchChangeHandler} />
+//       </div>
+//       <Users users={filteredUsers} />
+//     </Fragment>
+//   );
+// };
 
 export default UserFinder;
