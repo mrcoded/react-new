ee07e0063dba1e0ad4e2198984df9ab9d76fcd87
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-09 01:08:56 -0800

    feat: added ErrorBoundary to app

---
+++ react-class-errorboundary/src/components/ErrorBoundary.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class ErrorBoundary extends Component {
+  
+  constructor(){
+    super()
+    this.state = {
+        hasError: false
+    }
+  }  
+
+  componentDidCatch(error) {
+    //catch error
+    this.setState({hasError: true})
+  }
+  
+render() {
+    if(this.state.hasError){
+        return <p>Something went wrong</p>
+    }
+    return (
+      this.props.children
+    )
+  }
+}
+
+export default ErrorBoundary
