75b5e42a9ea087fda671ef9ce1e4e064a36a6ed6
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-15 20:35:00 -0800

    feat: returns if input is empty and added submit and indut function handler to set state

--- react-forms-input/src/components/SimpleInput.jsx
+++ react-forms-input/src/components/SimpleInput.jsx
@@ -1,9 +1,31 @@
+import { useRef, useState } from "react";
+
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef()
+  const [enteredName, setEnteredName] = useState('')
+
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value)
+  }
+
+  const formSubmissionHandler = e => {
+    e.preventDefault()
+    console.log(enteredName);
+
+    if (enteredName.trim() === '') return
+
+    //useref if u want to use the value once
+    const enteredValue = nameInputRef.current.value
+    //usestate for instant validation of keystrokes and to reset input
+    setEnteredName(" ")
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameInputRef} type='text' id='name' value={enteredName} onChange={nameInputChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
