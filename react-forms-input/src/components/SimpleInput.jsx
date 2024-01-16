88c648cc5842b08b52de7aa54729a99c29ffcb4a
CodedLibra <adeola.ogundijo@yahoo.com>
2024-01-15 23:51:28 -0800

    feat: added onblur event handling

--- react-forms-input/src/components/SimpleInput.jsx
+++ react-forms-input/src/components/SimpleInput.jsx
@@ -1,40 +1,63 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 const SimpleInput = (props) => {
   const nameInputRef = useRef()
   const [enteredName, setEnteredName] = useState('')
-  const [enteredNameIisValid, SetEnteredNameIisValid] = useState(true)
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false)
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log('Name Input is valid!');
+    }
+  }, [enteredNameIsValid]);
 
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value)
   }
 
-  const formSubmissionHandler = e => {
-    e.preventDefault()
-    console.log(enteredName);
+  const nameBlurInputHandler = e => {
+    setEnteredNameTouched(true)
 
     if (enteredName.trim() === '') {
-      SetEnteredNameIisValid(false)
+      setEnteredNameIsValid(false)
       return
     }
 
-    SetEnteredNameIisValid(true)
+  }
+
+  const formSubmissionHandler = e => {
+    e.preventDefault()
+    // console.log(enteredName);
+
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false)
+      return
+    }
+
+    setEnteredNameIsValid(true)
 
     //useref if u want to use the value once
     const enteredValue = nameInputRef.current.value
+    console.log(enteredValue);
+    // nameInputRef.current.value = ''; => NOT IDEAL, DON'T MANIPULATE THE DOM
     //usestate for instant validation of keystrokes and to reset input
     setEnteredName(" ")
   }
 
-  const nameInputClasses = enteredNameIisValid ? 'form-control' : 'form-control invalid'
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control'
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' value={enteredName} onChange={nameInputChangeHandler} />
-        {!enteredNameIisValid &&
+        <input ref={nameInputRef} type='text' id='name' onBlur={nameBlurInputHandler} value={enteredName} onChange={nameInputChangeHandler} />
+        {nameInputIsInvalid &&
           <p className="error-text">Name must not be empty.</p>
         }
       </div>
