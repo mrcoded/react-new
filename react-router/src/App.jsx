cefbfbc3b3998e19725948284bed69dd695b744d
CodedLibra <adeola.ogundijo@yahoo.com>
2024-02-25 01:28:15 -0800

    refactor routes path

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -2,18 +2,27 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Welcome from './components/Welcome';
 import Product from './components/Product';
+import AllQuotes from './pages/AllQuotes';
+import QuoteDetail from './pages/QuoteDetail';
+import NewQuote from './pages/NewQuote';
+import Comments from './components/comments/Comments';
+import Layout from './components/layout/Layout'
+import NotFound from './pages/NotFound';
 
 function App() {
 
   return (
-    <div>
-
+    <Layout>
       <Routes>
-        <Route path='/welcome' element={<Welcome />} />
-        <Route path='/products' element={<Product />} />
+        <Route path='/' exact element={<AllQuotes />} />
+        <Route path='/quotes' exact element={<AllQuotes />} />
+        <Route path='/quotes/:quoteId/*' element={<QuoteDetail />} />
+        <Route path='/quotes/:quoteId/comments' element={<Comments />} exact>
+        </Route>
+        <Route path='/new-quote' element={<NewQuote />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
-
-    </div >
+    </Layout>
   )
 }
 
