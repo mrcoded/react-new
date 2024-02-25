32ae492456b8453a8769ab1a01873a26597afddf
CodedLibra <adeola.ogundijo@yahoo.com>
2024-02-25 01:26:21 -0800

    feat: added usecallback to prevent re renders to addedcommenthandler function and fetched comments from api

--- react-router/src/components/comments/Comments.jsx
+++ react-router/src/components/comments/Comments.jsx
@@ -1,15 +1,53 @@
-import { useState } from 'react';
-
+import { useCallback, useEffect, useState } from 'react';
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
 import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
+import { useParams } from 'react-router-dom';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import CommentsList from './CommentsList';
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const { quoteId } = useParams();
+
+  const {
+    sendRequest, status, data: loadedComments
+  } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
-  
+
+  //useCb because the handler is passed as a useEffect dependecy props to NewComment
+  //and ensure the component is not recreated when the comment components is re evaluated
+  //and avoids unnecessary renders and infinite loops
+  const addedCommentHandler = useCallback(() => {
+    sendRequest(quoteId)
+  }, [sendRequest, quoteId]);
+
+  let comments;
+
+  if (status === 'pending') {
+    comments = (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (status === 'completed' && (loadedComments && loadedComments.length > 0)) {
+    comments = <CommentsList comments={loadedComments} />
+  }
+
+  if (status === 'completed' && (!loadedComments || loadedComments.length === 0)) {
+    comments = <p className='centered'>No comments were added yet!</p>
+  }
+
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
@@ -18,8 +56,12 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && <NewCommentForm
+        quoteId={quoteId}
+        onAddedComment={addedCommentHandler}
+      />
+      }
+      {comments}
     </section>
   );
 };
