241bf0f4ac9e98d7fc8dbbddf8e8a52f19b4a9be
CodedLibra <adeola.ogundijo@yahoo.com>
2024-02-26 00:21:23 -0800

    feat: initialized and added project files for reactAuth

---
+++ react-auth/src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+import StartingPageContent from '../component/StartingPage/StartingPageContent';
+
+const HomePage = () => {
+  return <StartingPageContent />;
+};
+
+export default HomePage;
---
+++ react-auth/src/pages/ProfilePage.jsx
@@ -0,0 +1,7 @@
+import UserProfile from '../component/profile/user-profile';
+
+const ProfilePage = () => {
+  return <UserProfile />;
+};
+
+export default ProfilePage;
