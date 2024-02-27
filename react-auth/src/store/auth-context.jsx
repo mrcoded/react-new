b171a9fe9e7e5c6ed025d541a0994e0b200908e4
CodedLibra <adeola.ogundijo@yahoo.com>
2024-02-27 01:27:45 -0800

    refactor: refactored authcontext logic

--- react-auth/src/store/auth-context.jsx
+++ react-auth/src/store/auth-context.jsx
@@ -18,53 +18,68 @@ const auth = getAuth(app);
 console.log(auth);
 
 const AuthContext = createContext({
-  token: "",
+  user: "",
+  setUser: "",
+  // loading: false
+  // token: "",
   isLoggedIn: false,
-  login: (token) => { },
-  logout: () => { }
+  // login: (user) => { },
+  // logout: () => { }
 });
 
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  // const [token, setToken] = useState(null);
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true)
 
   // !!token returns true if token is false and vice versa
-  const userIsLoggedIn = !!token;
+  const userIsLoggedIn = !!user;
+  // const userIsLoggedIn = !!token;
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      // setCurrentUser(user);
-      if (user) {
-        const idToken = user.getIdToken();
-        setToken(idToken);
-      } else {
-        setToken(null);
-      }
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setLoading(false)
+      if (currentUser) setUser(currentUser)
+      else { setUser(null) }
+      // if (user) {
+      //   const idToken = user.getIdToken();
+      //   setToken(idToken);
+      // } else {
+      //   setToken(null);
+      // }
     });
 
     return () => {
-      unsubscribe;
-      // if (unsubscribe) unsubscribe();
+      // unsubscribe;
+      if (unsubscribe) unsubscribe();
     }
   }, []);
 
-  const loginHandler = () => {
-    setToken(token);
-  }
+  // const loginHandler = () => {
+  //   if (user) {
+  //     setUser(user);
+  //     console.log(user);
+  //   } else {
+  //     console.log("no token found");
+  //   }
+  // }
 
-  const logoutHandler = () => {
-    setToken(null);
-  }
+  // const logoutHandler = () => {
+  //   setToken(null);
+  // }
 
   const contextValue = {
-    token: token,
+    // token: token,
     isLoggedIn: userIsLoggedIn,
-    login: loginHandler,
-    logout: logoutHandler
+    // login: loginHandler,
+    // logout: logoutHandler
+    user: user,
+    setUser: setUser
   }
 
   return (
     <AuthContext.Provider value={contextValue}>
-      {props.children}
+      {!loading && props.children}
     </AuthContext.Provider>
   );
 }
