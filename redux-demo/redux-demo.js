a69ce9e3647004c61084a7e1691dd1353b26b4c1
CodedLibra <adeola.ogundijo@yahoo.com>
2024-02-29 21:59:42 -0800

    feat: created redux store

---
+++ redux-demo/redux-demo.js
@@ -0,0 +1,62 @@
+const redux = require('redux');
+
+//reducer function - takes in old state and dispatched action
+//returns new state that is a pure function without side effects
+//like make HTTP call, store/fetch data in local storage
+
+//goal is to do same thing inside reducer with different actions
+
+const counterReducer = (state = {
+  //fallback defaultValue
+  counter: 0
+}, action) => {
+  if (action.type === 'increment') {
+    return {
+      //old counter runs when the store is created
+      counter: state.counter + 1
+    };
+  }
+  if (action.type === 'decrement') {
+    return {
+      //old counter runs when the store is created
+      counter: state.counter - 1
+    }
+  }
+
+  //default initilization action
+  return state;
+}
+
+//initialized store
+//pass which reducer will be changing value/manipulating data to the store
+const store = redux.createStore(counterReducer)
+
+//suscribed fn
+const counterSubscriber = () => {
+
+  // gives latest snapshot after state was updated 
+  const latestState = store.getState();
+  console.log(latestState);
+}
+
+//we point at the subscribe method fn
+store.subscribe(counterSubscriber)
+
+//dispatch an action (JS bject with a type property)
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
+
+const redux = require("redux");
+
+const counterReducer = (state, action) => {
+  return {
+    counter: state.counter + 1
+  };
+}
+const store = redux.createStore(counterReducer);
+
+const subscribe = () => {
+  const latestState = store.getState()
+}
+
+store.subscribe()
