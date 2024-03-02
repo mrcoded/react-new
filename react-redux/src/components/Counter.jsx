ec4b9eb5782b7dac18039f64646830864d74efd2
CodedLibra <adeola.ogundijo@yahoo.com>
2024-03-01 16:29:18 -0800

    feat: added useSelector to get a slice state, and useDispatch for an action from the store and added button to execute the actiions

--- react-redux/src/components/Counter.jsx
+++ react-redux/src/components/Counter.jsx
@@ -1,12 +1,35 @@
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  //use selector allows us to get a slice of the store
+  //changes to the store will cause the subscription to be be executed
+  const counter = useSelector(state => state.counter);
+
+  //dispatch fn calls actions against redux store
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" })
+  }
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" })
+  }
+
+  const toggleCounterHandler = () => { };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>
+        -- COUNTER VALUE --
+        {counter}
+      </div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
