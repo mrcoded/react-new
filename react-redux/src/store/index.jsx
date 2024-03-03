06dcd16c8754e669060583ef73f648c157027ce0
CodedLibra <adeola.ogundijo@yahoo.com>
2024-03-02 23:52:55 -0800

    feat: converted to redux toolkit setup

--- react-redux/src/store/index.jsx
+++ react-redux/src/store/index.jsx
@@ -1,42 +1,72 @@
-import { createStore } from "redux"
+import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true }
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === 'increment') {
-    return {
-      //never mutate the exisitn state in redux
-      //example state.counter++
-      //always copy and create a new object
-      counter: state.counter + 1,
-      showCounter: state.showCounter
+//React redux**************************
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === 'increment') {
+//     return {
+//never mutate the exisitn state in redux
+//example state.counter++
+//always copy and create a new object
+//     counter: state.counter + 1,
+//     showCounter: state.showCounter
+//   }
+// };
+
+// if (action.type === 'decrement') {
+//   return {
+//     counter: state.counter - 1,
+//     showCounter: state.showCounter
+//   }
+// };
+
+// if (action.type === 'increase') {
+//   return {
+//     counter: state.counter - action.payload,
+//     showCounter: state.showCounter
+//   }
+// };
+
+// if (action.type === 'toggle') {
+//   return {
+//     counter: state.counter,
+//     showCounter: !state.showCounter
+//   }
+// };
+
+//   return state;
+// }
+
+// const store = createStore(counterReducer);
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter
     }
-  };
+  }
+});
 
-  if (action.type === 'decrement') {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter
-    }
-  };
 
-  if (action.type === 'increase') {
-    return {
-      counter: state.counter - action.payload,
-      showCounter: state.showCounter
-    }
-  };
+//configStore makes combining multiple reducers ino one easier
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
-  if (action.type === 'toggle') {
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter
-    }
-  };
-
-  return state;
-}
-
-const store = createStore(counterReducer);
+//Action creator
+export const counterActions = counterSlice.actions
 
 export default store;
