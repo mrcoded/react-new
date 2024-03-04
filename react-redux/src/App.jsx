5989f9733e8dd4653fb154bfb728cbf6aca3c0e3
CodedLibra <adeola.ogundijo@yahoo.com>
2024-03-03 16:31:48 -0800

    feat: aadded new react components for redux aysnc

--- react-redux/src/App.jsx
+++ react-redux/src/App.jsx
@@ -1,20 +1,37 @@
-import Counter from './components/Counter';
-import Auth from './components/Auth'
-import Header from './components/Header'
-import UserProfile from './components/UserProfile'
-import { useSelector } from 'react-redux';
+import Cart from './componentb/Cart/Cart';
+import Layout from './componentb/Layout/Layout';
+import Products from './componentb/Shop/Products';
 
 function App() {
-  const isAuth = useSelector(state => state.auth.isAuth)
-
   return (
-    <>
-      <Header />
-      {!isAuth && <Auth />}
-      {isAuth && <UserProfile />}
-      <Counter />
-    </>
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
   );
 }
 
 export default App;
+
+//*************Components A*************/
+// import Counter from './components/Counter';
+// import Auth from './components/Auth'
+// import Header from './components/Header'
+// import UserProfile from './components/UserProfile'
+// import { useSelector } from 'react-redux';
+
+// function App() {
+//   const isAuth = useSelector(state => state.auth.isAuth)
+
+//   return (
+//     <>
+//       <Header />
+//       {!isAuth && <Auth />}
+//       {isAuth && <UserProfile />}
+//       <Counter />
+//     </>
+//   );
+// }
+
+// export default App;
+
