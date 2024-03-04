d1f74b59adb6d82b3bbdb1ea6fb6e37ab90efeda
CodedLibra <adeola.ogundijo@yahoo.com>
2024-03-04 01:00:18 -0800

    feat: added toggleHandler to dispatch actions for ui conditional rendering

--- react-redux/src/componentb/Cart/CartButton.jsx
+++ react-redux/src/componentb/Cart/CartButton.jsx
@@ -1,8 +1,16 @@
+import { uiActions } from '../../storeb/ui-slice';
 import classes from './CartButton.module.css';
+import { useDispatch } from 'react-redux'
 
 const CartButton = (props) => {
+  const dispatch = useDispatch()
+
+  const toggleHandler = () => {
+    dispatch(uiActions.toggle())
+  }
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
