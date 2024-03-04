793b9e4bcf440e8de864e8fd044e6b560632f211
CodedLibra <adeola.ogundijo@yahoo.com>
2024-03-04 15:24:11 -0800

    refactor: corrected addItemToCart method name and renaed itemId to id in cart-slice

--- react-redux/src/storeb/cart-slice.js
+++ react-redux/src/storeb/cart-slice.js
@@ -7,12 +7,14 @@ const cartSlice = createSlice({
     totalQuantity: 0
   },
   reducers: {
-    addToCart(state, action) {
+    addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find(item => item.id === newItem.id)
+      state.totalQuantity++;
+
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           name: newItem.title,
           price: newItem.price,
           quantity: 1,
@@ -22,10 +24,22 @@ const cartSlice = createSlice({
         existingItem.quantity++;
         existingItem.totalPrice = existingItem.totalPrice + newItem.price
       }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find(item => item.id === id)
+      state.totalQuantity--;
+
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id)
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
     }
   }
 })
 
-export const uiActions = cartSlice.actions
+export const cartActions = cartSlice.actions
 
-export default cacarSlice;
+export default cartSlice;
