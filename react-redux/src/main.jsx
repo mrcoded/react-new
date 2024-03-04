7b428a589b7d530dffc6257c683dc3e4e253f478
CodedLibra <adeola.ogundijo@yahoo.com>
2024-03-04 00:59:28 -0800

    refactor

--- react-redux/src/main.jsx
+++ react-redux/src/main.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import App from './App.jsx'
 import './index.css'
 
-import store from '../src/store/index.jsx'
+import store from '../src/storeb/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
