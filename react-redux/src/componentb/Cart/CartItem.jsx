2bba60dda5858c4e3fe53aee45d4ee2ad3414d41
CodedLibra <adeola.ogundijo@yahoo.com>
2024-03-04 15:28:50 -0800

    feat: added addItemHandler function to button

--- react-redux/src/componentb/Cart/CartItem.jsx
+++ react-redux/src/componentb/Cart/CartItem.jsx
@@ -1,7 +1,23 @@
+import { useDispatch } from 'react-redux';
 import classes from './CartItem.module.css';
+import { cartActions } from '../../storeb/cart-slice';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const dispatch = useDispatch()
+  const { id, title, quantity, total, price } = props.item;
+
+  const addItemHandler = () => {
+    dispatch(cartActions.addItemToCart({
+      id,
+      title,
+      price
+    }))
+  }
+
+  const removeItemHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id))
+
+  }
 
   return (
     <li className={classes.item}>
@@ -17,8 +33,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
         </div>
       </div>
     </li>
