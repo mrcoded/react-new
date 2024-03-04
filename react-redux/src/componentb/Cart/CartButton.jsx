31763c82b1613307589256b2b6d2d4ecf78921b5
CodedLibra <adeola.ogundijo@yahoo.com>
2024-03-04 15:25:19 -0800

    feat: added cartQuantity state

--- react-redux/src/componentb/Cart/CartButton.jsx
+++ react-redux/src/componentb/Cart/CartButton.jsx
@@ -1,9 +1,10 @@
 import { uiActions } from '../../storeb/ui-slice';
 import classes from './CartButton.module.css';
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 const CartButton = (props) => {
   const dispatch = useDispatch()
+  const cartQuantity = useSelector(state => state.cart.totalQuantity);
 
   const toggleHandler = () => {
     dispatch(uiActions.toggle())
@@ -12,7 +13,7 @@ const CartButton = (props) => {
   return (
     <button className={classes.button} onClick={toggleHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
