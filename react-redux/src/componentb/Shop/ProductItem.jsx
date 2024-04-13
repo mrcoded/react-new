505b3f4da6ce4acf10c7b5e0b089035aec362b92
CodedLibra <adeola.ogundijo@yahoo.com>
2024-04-13 00:31:48 -0700

    feat: added addItemToCart slice

--- react-redux/src/componentb/Shop/ProductItem.jsx
+++ react-redux/src/componentb/Shop/ProductItem.jsx
@@ -1,18 +1,50 @@
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 import { cartActions } from '../../storeb/cart-slice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const ProductItem = (props) => {
+  const cart = useSelector(state => state.cart)
   const dispatch = useDispatch();
+
   const { title, price, description, id } = props;
 
   const addToCartHandler = () => {
+    //do not mutate directly  in a reducer or redux
+    // const newTotalQuantity = cart.totalQuantity + 1;
+
+    // const updatedItems = cart.items.slice(); // create copy via slice to avoid mutating original state
+    // const existingItem = updatedItems.find((item) => item.id === id);
+    // if (existingItem) {
+    //   const updatedItem = { ...existingItem }; // new object + copy existing properties to avoid state mutation
+    //   updatedItem.quantity++;
+    //   updatedItem.totalPrice = updatedItem.totalPrice + price;
+    //   const existingItemIndex = updatedItems.findIndex(
+    //     (item) => item.id === id
+    //   );
+    //   updatedItems[existingItemIndex] = updatedItem;
+    // } else {
+    //   updatedItems.push({
+    //     id: id,
+    //     price: price,
+    //     quantity: 1,
+    //     totalPrice: price,
+    //     name: title,
+    //   });
+    // }
+
+    // const newCart = {
+    //   totalQuantity: newTotalQuantity,
+    //   items: updatedItems,
+    // };
+
+    // dispatch(cartActions.replaceCart(newCart));
+
     dispatch(cartActions.addItemToCart({
       id,
       title,
       price,
-      // description
+      description
     }))
   }
 
